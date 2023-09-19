a9573c99811dc11291a9853590b7937b23a17194
Mai2aa <mysaadil0@gmail.com>
2023-09-18 19:58:49 -0700

    edit

--- main.h
+++ main.h
@@ -17,8 +17,8 @@
 #define S_SHORT 1
 
 /**
- * fmt struct - struct 
- * @ch: character
+ * struct fmt - struct of format
+ * @form: character
  * @fn: pointer to fun
  */
 struct fmt
@@ -26,20 +26,29 @@ struct fmt
 	char form;
 	int (*fn)(va_list, char[], int, int, int, int);
 };
+
 typedef struct fmt fmt_t;
 
 
 
 /* task 0 */
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precision, int size);
-int print_ch(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_str(va_list types, char buffer[], int flags, int width, int precision, int size);
-int print_per(va_list types, char buffer[], int flags, int width, int precision, int size);
-int handle_ch(char c, char buffer[], int flags, int width, int precision, int size);
-int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
+int handle_print(const char *fmt, int *i,
+		va_list list, char buffer[], int flags, int width, int precision, int size);
+int print_ch(va_list list1, char buffer[],
+		int flags, int width, int precision, int size);
+int print_str(va_list list1, char buffer[],
+		int flags, int width, int precision, int size);
+int print_per(va_list list1, char buffer[],
+		int flags, int width, int precision, int size);
+int handle_ch(char c, char buffer[],
+		int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+		int flags, int width, int precision, int size);
+int write_num(int ind, char bff[],
+		int flags, int width, int precision, int length, char padd, char extra_c);
 long int convert_size_number(long int num, int size);
 int is_digit(char);
 /* task 7, 11, 12 */
